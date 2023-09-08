a869dc14ce778603fc8ab85cc8c3dd9854448332
matias <matias.rinaudo@avenida.com>
2023-09-08 11:42:31 -0300

    feat: add job update shipping status in schedule.rb

--- app/jobs/update_shipping_status_job.rb
+++ app/jobs/update_shipping_status_job.rb
@@ -2,6 +2,15 @@ class UpdateShippingStatusJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    # IMPLEMENT ME
+    orders = Order.where.not(fedex_id: nil)
+
+    orders.map do |order|
+      shipment = Fedex::Shipment.find(order.fedex_id) 
+      order.shipping_method = shipment.status
+
+      OrderMailer.success_update_status_order(order_id: order.id).deliver_now
+
+      order.save
+    end
   end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
 every 15.min do
   rake 'update_shipping_status_job'
 end
+
+every 15.min, :at => '4:30 am' do
+  runner UpdateShippingStatusJob.perform_async
+end
